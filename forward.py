93f83629ed82f270d0af3a1cacb20c8527729492
Marcus Crane <marcus@thingsima.de>
2017-10-12 15:09:35 +1300

    Hardcoded forwarding works. Gotta head out bleh

--- forward.py
+++ forward.py
@@ -2,6 +2,7 @@ import configparser
 import re
 from base64 import b64encode
 
+from flask import Flask, Response
 import requests
 from requests.auth import HTTPBasicAuth
 
@@ -9,6 +10,23 @@ def loadConfig():
   config = configparser.ConfigParser()
   config.read('config.ini')
   return config
+
+app = Flask(__name__)
+
+@app.route('/')
+def fetchPodcast():
+  cfg = loadConfig()
+  username = cfg['account']['username']
+  password = cfg['account']['password']
+
+  r = requests.get('http://www.f4wradio.com/podcast/100517wo.mp3', auth=HTTPBasicAuth(username, password), stream=True)
+  
+  def bufferStream():
+    for chunk in r.iter_content(chunk_size=128):
+      yield chunk
+  return Response(bufferStream(), mimetype='audio/mpeg')
+
+
 
 # def fetchFeed():
 #   r = requests.get('http://www.f4wradio.com/feed/wor.rss')
@@ -18,15 +36,3 @@ def loadConfig():
 #   feed = fetchFeed()
 #   pattern = "http://www.f4wradio.com"
 #   feedUpdate = re.sub(pattern, "http://f4w.thingsima.de", feed)
-
-def generateStream(id):
-  cfg = loadConfig()
-  username = cfg['account']['username']
-  password = cfg['account']['password']
-
-  r = requests.get(f'http://www.f4wradio.com/podcast/{id}.mp3', auth=HTTPBasicAuth(username, password), stream=True)
-  with open(f'{id}.mp3', 'wb') as podcast:
-    for chunk in r.iter_content(chunk_size=128):
-      podcast.write(chunk)
-
-generateStream("100917wo")
