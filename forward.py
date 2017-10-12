fc702b250fd8be2beae381599f423f32867c5076
Marcus Crane <marcus@thingsima.de>
2017-10-12 18:40:00 +1300

    The actual script itself is now on par with the original

--- forward.py
+++ forward.py
@@ -11,28 +11,37 @@ def loadConfig():
   config.read('config.ini')
   return config
 
+def cfg(key, option):
+  config = loadConfig()
+  return config[key][option] 
+
+def fetchFeed():
+  r = requests.get('http://www.f4wradio.com/feed/wor.rss')
+  return r.text
+
 app = Flask(__name__)
 
 @app.route('/')
-def fetchPodcast():
-  cfg = loadConfig()
-  username = cfg['account']['username']
-  password = cfg['account']['password']
+def index():
+  return Response("Visit <a href='/wor.rss'>wor.rss</a>", mimetype="text/html")
 
-  r = requests.get('http://www.f4wradio.com/podcast/100517wo.mp3', auth=HTTPBasicAuth(username, password), stream=True)
-  
+@app.route('/wor.rss')
+def updateFeedURLs():
+  feed = fetchFeed()
+  domain = cfg('server', 'domain')
+  origin = "http://www.f4wradio.com"
+
+  updatedFeed = re.sub(origin, domain, feed)
+  return Response(updatedFeed, mimetype='text/xml')
+
+@app.route('/podcast/<id>.mp3')
+def fetchPodcast(id):
+  username = cfg('account', 'username')
+  password = cfg('account', 'password')
+
+  r = requests.get(f'http://www.f4wradio.com/podcast/{id}.mp3', auth=HTTPBasicAuth(username, password), stream=True)
+
   def bufferStream():
     for chunk in r.iter_content(chunk_size=128):
       yield chunk
   return Response(bufferStream(), mimetype='audio/mpeg')
-
-
-
-# def fetchFeed():
-#   r = requests.get('http://www.f4wradio.com/feed/wor.rss')
-#   return r.text
-
-# def updateFeed():
-#   feed = fetchFeed()
-#   pattern = "http://www.f4wradio.com"
-#   feedUpdate = re.sub(pattern, "http://f4w.thingsima.de", feed)
