995dfeec7655e77105bc6394840547876e114df4
Marcus Crane <marcus@thingsima.de>
2017-10-12 21:07:54 +1300

    Now uses the config port

--- forward.py
+++ forward.py
@@ -48,4 +48,4 @@ def fetchPodcast(id):
   return Response(bufferStream(), mimetype='audio/mpeg')
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0', port=cfg('server', 'port'))
